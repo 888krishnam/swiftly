9ed7d150202e3ae046d61f93e1f893d3abb1983b
Krishnam Agarwal <888krishnam@gmail.com>
2025-07-03 12:19:05 +0530

    Background AI Jobs Setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,21 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import { useTRPC } from "@/trpc/client";
+import { useState } from "react";
 import { useMutation } from "@tanstack/react-query";
+import { useTRPC } from "@/trpc/client";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
 
 const Page = () => {
+  const [value, setValue] = useState("");
   const trpc = useTRPC();
   const invoke = useMutation(trpc.invoke.mutationOptions({}));
   return (
     <>
-      <h1>Test</h1>
-      <Button onClick={ () => invoke.mutate({ text: "hello" }) }>
+      <Input value={value} onChange={(e) => setValue(e.target.value)}/>
+      <Button onClick={ () => invoke.mutate({ value: value }) }>
         Invoke Background Job
       </Button>
     </>
