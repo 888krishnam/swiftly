bf47944fc723247a6ccf6d3a3d3a331e5a8200aa
Krishnam Agarwal <888krishnam@gmail.com>
2025-07-08 11:41:49 +0530

    Project Schema

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -152,6 +152,7 @@ export const codeAgentFunction = inngest.createFunction(
       if (isError) {
         return await prisma.message.create({
           data: {
+            projectId: event.data.projectId,
             content: "An error occurred while processing your request.",
             role: "ASSISTANT",
             type: "ERROR",
@@ -161,6 +162,7 @@ export const codeAgentFunction = inngest.createFunction(
 
       return await prisma.message.create({
         data: {
+          projectId: event.data.projectId,
           content: result.state.data.summary || "",
           role: "ASSISTANT",
           type: "RESULT",
