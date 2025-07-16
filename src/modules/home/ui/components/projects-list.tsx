d3f3b8899a7dd19bf428956c28e4d26ebcaeba9b
Krishnam Agarwal <888krishnam@gmail.com>
2025-07-16 16:04:28 +0530

    Add Authentication

--- src/modules/home/ui/components/projects-list.tsx
+++ src/modules/home/ui/components/projects-list.tsx
@@ -6,20 +6,24 @@ import { formatDistanceToNow } from "date-fns";
 import { useQuery } from "@tanstack/react-query";
 import { useTRPC } from "@/trpc/client";
 import { Button } from "@/components/ui/button";
+import { useUser } from "@clerk/nextjs";
 
 const ProjectsList = () => {
   const trpc = useTRPC();
+  const { user } = useUser();
   const { data: projects } = useQuery(trpc.projects.getMany.queryOptions());
+
+  if (!user) return null;
 
   return (
     <div className="w-full bg-white dark:bg-sidebar rounded-xl p-8 border flex flex-col gap-y-6 sm:gap-y-4">
         <h2 className="text-2xl font-semibold">
-          Projects
+          { user?.firstName }&apos;s Projects
         </h2>
         <div className="grid grid-cols-1 sm:grid-cols-3 gap-6">
           {projects?.length === 0 ? (
             <div className="col-span-full text-center">
-              <p className="text-sm text-muted-foreground">No projects found</p>
+              <p className="text-sm text-muted-foreground">Create Projects to list them here.</p>
             </div>
           ) : (
             projects?.map((project) => (
