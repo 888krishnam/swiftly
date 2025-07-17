d991abaf24a683b806c28fd7325be8bcc7e53ff8
Krishnam Agarwal <888krishnam@gmail.com>
2025-07-17 15:59:31 +0530

    sandbox over https

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -201,7 +201,7 @@ export const codeAgentFunction = inngest.createFunction(
     const sandboxUrl = await step.run("get-sandbox-url", async () => {
       const sandbox = await getSandbox(sandboxId);
       const host = sandbox.getHost(3000);
-      return `http://${host}`;
+      return `https://${host}`;
     });
 
     // Save the assistant’s message + fragment record
